6805dc1888d357d34648a36a15b9b51b074d0788
Seth Singletary <singletaryseth8@gmail.com>
2022-05-07 21:31:59 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Seth Singletary on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
