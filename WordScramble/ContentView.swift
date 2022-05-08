7e931ceba8be4c355147ffbdee96875ce6043f5f
Seth Singletary <singletaryseth8@gmail.com>
2022-05-08 18:59:44 -0400

    Update ContentView.swift

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section{
+                    ForEach(usedWords, id: \.self){ word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+    func addNewWord(){
+        //lowercase and trim the word so there won't be duplicates with small diffrenses.
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //exit if the remaining string is empty
+        guard answer.count > 0 else{return}
+        
+        //extra validation to come
+        withAnimation(){
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
     }
 }
 
